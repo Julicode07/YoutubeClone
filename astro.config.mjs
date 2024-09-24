08c8d50d053e794251bc31cc644846ace79923b3
JuliCode07 <Julicode07@gmail.com>
2024-09-24 14:13:36 -0500

    Add search in navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   integrations: [tailwind(), react()],
   devToolbar: {
     enabled: false,
@@ -15,6 +16,6 @@ export default defineConfig({
       alias: {
         "@": "/src",
       },
-    }
-  }
+    },
+  },
 });
