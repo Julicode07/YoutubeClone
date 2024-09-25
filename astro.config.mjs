6b3a2727568e0de8da54140671b26e8bac8ee178
JuliCode07 <Julicode07@gmail.com>
2024-09-25 14:36:15 -0500

    Add adapter node

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,19 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://julicode07.github.io",
   base: "/YoutubeClone",
   output: "server",
   integrations: [tailwind(), react()],
+
   devToolbar: {
     enabled: false,
   },
+
   vite: {
     resolve: {
       alias: {
@@ -20,4 +24,8 @@ export default defineConfig({
       },
     },
   },
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
