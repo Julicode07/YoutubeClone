bf872ee5c7f301cdaf05adc4e776945385c4b83e
JuliCode07 <Julicode07@gmail.com>
2024-09-25 14:23:45 -0500

    Add deploy Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://julicode07.github.io",
+  base: "/YoutubeClone",
   output: "server",
   integrations: [tailwind(), react()],
   devToolbar: {
